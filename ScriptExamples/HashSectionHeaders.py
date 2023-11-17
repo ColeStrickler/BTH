ac88c2b236733bdac8625fe04a88c263e756a52b
Cole Strickler <82488869+ColeStrickler@users.noreply.github.com>
2023-11-17 12:54:08 -0600

    Add files via upload

---
+++ ScriptExamples/HashSectionHeaders.py
@@ -0,0 +1,32 @@
+import mgr
+import hashlib
+
+mgr.LoadFile('C:\\Windows\\notepad.exe')
+
+def hash_bytes(data):
+    # Create a new SHA-256 hash object
+    md5 = hashlib.md5()
+    # Update the hash object with the bytes to be hashed
+    md5.update(data)
+
+    # Get the hexadecimal representation of the hash
+    hash_result = md5.hexdigest()
+
+    return hash_result
+
+
+
+
+sh = mgr.SectionHeaders()
+
+
+for dir in sh:
+	dir_start = mgr.rvaToRaw(dir.VirtualAddress)
+	dir_size = dir.SizeOfRawData
+	section_bytes = b''
+	for i in range(dir_size):
+		byte = bytes.fromhex(mgr.GetByte(dir_start + i))
+		
+		section_bytes += byte
+	print(f'{dir.Name}:\t{hash_bytes(section_bytes)}')
+	
